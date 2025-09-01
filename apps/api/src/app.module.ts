593c7b4768a10d26a5e923eb4a75e3df39d5bbac
diegomadariaga <diegonicolasmadariaga@gmail.com>
2025-09-01 14:20:00 -0400

    feat: update TypeORM configuration to use specific YoutubeJobEntity for improved entity management

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { YoutubeModule } from './youtube/youtube.module.js';
+import { YoutubeJobEntity } from './youtube/job.entity.js';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'data.sqlite',
-      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      entities: [YoutubeJobEntity],
       synchronize: true,
     }),
     YoutubeModule
